c2ceaade5b67d08fd2ff13e881afc159ae0f359a
jade <86130123+7f-galaxii@users.noreply.github.com>
2025-02-16 11:40:50 -0500

    more link fail

--- radio/header.js
+++ radio/header.js
@@ -1,8 +1,8 @@
 let appHeader = `
     <h1>I Wanna Radio</h1>
-    <a href="/">[main]</a>
-    <!-- <a href="/requests">[requests]</a> -->
+    <a href="/radio/">[main]</a>
+    <!-- <a href="/radio/requests">[requests]</a> -->
     <a href="#" onclick="javascript:alert('This doesn\'t exist right now, sorry!');">[discord]</a>
-    <a href="/about">[about]</a>
+    <a href="/radio/about">[about]</a>
 `;
 document.getElementById("app-header").innerHTML = appHeader;
