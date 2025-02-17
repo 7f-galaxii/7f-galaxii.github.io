83243303044857df357f292c05b3aa0892d5fa7c
jade <86130123+7f-galaxii@users.noreply.github.com>
2025-02-17 07:32:53 -0500

    extremely minor updates

--- radio/header.js
+++ radio/header.js
@@ -2,7 +2,8 @@ let appHeader = `
     <h1>I Wanna Radio</h1>
     <a href="/radio/">[main]</a>
     <!-- <a href="/radio/requests">[requests]</a> -->
-    <a href="#" onclick="javascript:alert('This doesn\'t exist right now, sorry!');">[discord]</a>
+    <a id="discord-link" target="_blank" href="#">[discord]</a>
     <a href="/radio/about">[about]</a>
 `;
 document.getElementById("app-header").innerHTML = appHeader;
+document.getElementById("discord-link").setAttribute("href", "https://dis"+"cord.gg/NG"+"cRajCNa5"); // really basic attempt at trying to stop primitive scanners/bots
